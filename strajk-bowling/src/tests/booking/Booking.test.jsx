9667229dc3de2673288127480471c24549603652
robjor88 <robjor88@gmail.com>
2025-12-07 17:29:54 +0100

    antal skor stämmer överens med antal spelare

--- strajk-bowling/src/tests/booking/Booking.test.jsx
+++ strajk-bowling/src/tests/booking/Booking.test.jsx
@@ -27,6 +27,7 @@ const getInputs = () => ({
   dateInput: document.querySelector("input[name='when']"),
   timeInput: document.querySelector("input[name='time']"),
   peopleInput: document.querySelector("input[name='people']"),
+  lanesInput: document.querySelector("input[name='lanes']"),
   submitButton: screen.findByRole("button", { name: /strIIIIIike!/i }),
 });
 
@@ -56,6 +57,37 @@ describe("Integration Tests: Booking Flow (VG Requirement", () => {
 
     expect(
       await screen.findByText(/Alla fälten måste vara ifyllda/i)
+    ).toBeInTheDocument();
+    expect(mockedNavigate).not.toHaveBeenCalled();
+  });
+
+  test("Visar felmeddelande om antalet skor inte stämmer överens med antal spelare", async () => {
+    render(
+      <BrowserRouter>
+        <Booking />
+      </BrowserRouter>
+    );
+
+    const { dateInput, timeInput, peopleInput, lanesInput } = getInputs();
+
+    const submitButton = await screen.findByRole("button", {
+      name: /strIIIIIike!/i,
+    });
+
+    const addShoeButton = screen.getByRole("button", { name: "+" });
+
+    await userEvent.type(dateInput, "2026-06-06");
+    await userEvent.type(timeInput, "18:00");
+    await userEvent.type(lanesInput, "1");
+    await userEvent.type(peopleInput, "2");
+
+    await userEvent.click(addShoeButton);
+    await userEvent.click(submitButton);
+
+    expect(
+      await screen.findByText(
+        /Antalet skor måste stämma överens med antal spelare/i
+      )
     ).toBeInTheDocument();
     expect(mockedNavigate).not.toHaveBeenCalled();
   });
